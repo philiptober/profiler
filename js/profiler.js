666074de21ef60f12f2d734ae7c99ae0b3ab27a3
Philip Tober <philiptober@gmail.com>
2014-08-16 11:31:18 +0200

    added roles select

--- js/profiler.js
+++ js/profiler.js
@@ -1,3 +1,14 @@
+function contains(targetSubString, targetString) 
+{
+	if ( targetString === undefined || targetString == null )
+		return false;
+		
+	if ( targetString.indexOf(targetSubString) >= 0 )
+		return true;
+	else
+		return false;
+}
+
 $( document ).on('click', '[data-multiplefor]', function() 
 {
 	var targets = $(this).attr("data-multiplefor").split(',');
@@ -7,4 +18,44 @@ $( document ).on('click', '[data-multiplefor]', function()
 		$('#' + targets[j]).prop('checked',
 			function(i, oldVal) { return !oldVal; });
 	}
+});
+
+$( document ).on('click', 'ul.profiler > li > div.view-roles > ul > li.roles-add', function()
+{
+	var self = $( this );
+	
+	var marker = self.parent().parent().parent();
+	var markerClasses = marker.attr('class');
+	
+	if ( contains('checked', markerClasses) )
+		marker.removeClass('checked');
+	else
+		marker.addClass('checked');
+});
+
+$( document ).on('click', 'ul.profiler > li > div.view-roles > ul:nth-of-type(2) > li', function() {
+	var self = $( this );
+	var selfClasses = self.attr('class');
+	var selfValue = self.attr('value');
+	
+	var mirrorDirective = self.parent().parent().children('ul:first-of-type');
+	var mirror = mirrorDirective.children('li[value=\"' + selfValue + '\"]');
+	
+	if ( contains('checked', selfClasses) )
+	{
+		if (mirror.length > 0)
+			mirror.fadeOut('slow', function() {$(this).remove()});
+			
+		self.removeClass('checked');
+	}
+	else
+	{
+		$(self.prop('outerHTML')).hide().prependTo(mirrorDirective).fadeIn('slow');
+	
+		self.addClass('checked');
+	}
+});
+
+$( document ).on('change', 'ul.profiler > li > div.view-image > input', function() {
+	$( this ).parent().children('label')[0].innerText = $( this )[0].files[0].name;
 });
